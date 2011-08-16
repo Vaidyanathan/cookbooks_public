c5946bf5e2012ab374bc94a4d2896b60163f7062
Chris Fordham <chris@xhost.com.au>
2011-08-16 16:02:27 +1000

    Change defaults for optional setup_hostname attributes from '' to nil.

--- cookbooks/rs_utils/metadata.rb
+++ cookbooks/rs_utils/metadata.rb
@@ -17,7 +17,6 @@ recipe "rs_utils::install_tools", "Install RightScale instance tools"
 recipe "rs_utils::install_mysql_collectd_plugin", "Install mysql collectd plugin"
 recipe "rs_utils::install_file_stats_collectd_plugin", "Install file-stats.rb collectd plugin.  This is used for mysql binary backup alerting."
 
-
 attribute "rs_utils/timezone",
   :display_name => "Timezone",
   :description => "Sets the system time to the timezone of the specified input, which must be a valid zoneinfo/tz database entry.  If the input is 'unset' the timezone will use the 'localtime' that's defined in your RightScale account under Settings -> User -> Preferences tab.  You can find a list of valid examples from the timezone pulldown bar in the Preferences tab. The server will not be updated for daylight savings time.  Ex: US/Pacific, US/Eastern",
@@ -64,12 +63,12 @@ attribute "rs_utils/domain_name",
   :display_name => "Domain Name",
   :description => "The domain name that you would like this node to have, e.g. domain.suf",
   :required => "optional",
-  :default => "" ,
+  :default => nil ,
   :recipes => [ "rs_utils::setup_hostname" ]
 
 attribute "rs_utils/search_suffix",
   :display_name => "Domain Search Suffix",
   :description => "The domain search suffix you would like this node to have, e.g. domain.suf.",
   :required => "optional",
-  :default => "",
+  :default => nil,
   :recipes => [ "rs_utils::setup_hostname" ]
