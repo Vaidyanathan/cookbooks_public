82d8b29a5d68f1ccbd897a7ad87887b66766cbed
Chris Fordham <chris@xhost.com.au>
2011-08-16 17:02:27 +1000

    Use unless for pre-set of entries for /etc/hosts.

--- cookbooks/rs_utils/recipes/setup_hostname.rb
+++ cookbooks/rs_utils/recipes/setup_hostname.rb
@@ -45,7 +45,7 @@ def show_host_info
 end
 
 # set hostname from short or long (when domain_name set)
-if node.rs_utils.domain_name != nil || node.rs_utils.domain_name != ''
+unless node.rs_utils.domain_name.nil? || node.rs_utils.domain_name == ''  
   hostname = "#{node.rs_utils.short_hostname}.#{node.rs_utils.domain_name}"
   hosts_list = "#{node.rs_utils.short_hostname}.#{node.rs_utils.domain_name} #{node.rs_utils.short_hostname}"
 else
