a2ecef2415cc0888724276d495b79dadf124c88b
Chris Fordham <chris@xhost.com.au>
2011-08-19 17:32:29 +1000

    Revert private_ssh_key to "" default from nil.

--- cookbooks/rs_utils/attributes/default.rb
+++ cookbooks/rs_utils/attributes/default.rb
@@ -32,7 +32,7 @@
 set_unless[:rs_utils][:timezone] = "UTC"    
 set_unless[:rs_utils][:process_list] = ""
 set_unless[:rs_utils][:process_match_list] = ""
-set_unless[:rs_utils][:private_ssh_key] = nil
+set_unless[:rs_utils][:private_ssh_key] = ""
 set_unless[:rs_utils][:collectd_share] = "/usr/share/collectd"
 
 set_unless[:rs_utils][:mysql_binary_backup_file] = "/var/run/mysql-binary-backup"
