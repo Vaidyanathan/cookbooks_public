1a91bcbcdb5f777a446f1429423a7a3347c28f48
Chris Fordham <chris@xhost.com.au>
2011-08-19 17:39:32 +1000

    Use if instead of unless to check existance of key.

--- cookbooks/rs_utils/recipes/setup_ssh.rb
+++ cookbooks/rs_utils/recipes/setup_ssh.rb
@@ -22,7 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-unless node.private_ssh_key.nil? || node.private_ssh_key == ''  
+if node.private_ssh_key.nil? || node.private_ssh_key == ''  
 
   log "Install private key"
   private_ssh_key = node.private_ssh_key
