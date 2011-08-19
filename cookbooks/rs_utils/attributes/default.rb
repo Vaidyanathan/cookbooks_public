5d86b8825e27e803f9d811e26f674ff9d1608822
Chris Fordham <chris@xhost.com.au>
2011-08-19 18:48:38 +1000

    Test blank default values.

--- cookbooks/rs_utils/attributes/default.rb
+++ cookbooks/rs_utils/attributes/default.rb
@@ -30,13 +30,9 @@
 # Optional attributes
 #
 set_unless[:rs_utils][:timezone] = "UTC"    
-set_unless[:rs_utils][:process_list] = ""
-set_unless[:rs_utils][:process_match_list] = ""
 set_unless[:rs_utils][:collectd_share] = "/usr/share/collectd"
-
 set_unless[:rs_utils][:mysql_binary_backup_file] = "/var/run/mysql-binary-backup"
 
-default[:rs_utils][:plugin_list] = ""
 default[:rs_utils][:plugin_list_ary] = [
   "cpu",
   "df",
@@ -53,6 +49,9 @@ default[:rs_utils][:process_list] = ""
 default[:rs_utils][:process_list_ary] = []
 
 default[:rs_utils][:private_ssh_key] = ""
+default[:rs_utils][:process_match_list] = ""
+default[:rs_utils][:process_list] = ""
+default[:rs_utils][:plugin_list] = ""
 
 #
 # Setup Distro dependent variables
