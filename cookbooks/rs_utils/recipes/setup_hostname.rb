7c3f636e5b7041b49a514a8f17abca72b0d31941
Chris Fordham <chris@xhost.com.au>
2011-09-06 21:33:41 +1000

    Use and instead of or for resolv.conf logic.

--- cookbooks/rs_utils/recipes/setup_hostname.rb
+++ cookbooks/rs_utils/recipes/setup_hostname.rb
@@ -91,7 +91,7 @@ log 'Configuring /etc/resolv.conf.'
 # assumes the minimum option(s) in resolv.conf is a namserver
 nameserver = "nameserver #{`cat /etc/resolv.conf | grep -v '^#' | grep nameserver | awk '{print $2}' | tr -d '\n'`}"
 
-if !node.rs_utils.search_suffix.nil? or node.rs_utils.search_suffix != ""
+if !node.rs_utils.search_suffix.nil? and node.rs_utils.search_suffix != ""
   search = "search #{node.rs_utils.search_suffix}"
 else
   current_search = "#{`cat /etc/resolv.conf | grep -v '^#' | grep search | awk '{print $2}' | tr -d '\n'`}"
