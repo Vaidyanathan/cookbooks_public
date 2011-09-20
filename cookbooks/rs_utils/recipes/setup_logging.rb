ebb801e3a10b5a733ccba0b186bb2f8b5c1ac210
Chris Fordham <chris@xhost.com.au>
2011-09-20 13:12:40 +1000

    Test new logic for graceful exist in non-rightscale env.

--- cookbooks/rs_utils/recipes/setup_logging.rb
+++ cookbooks/rs_utils/recipes/setup_logging.rb
@@ -27,73 +27,63 @@
 # Can not setup other clouds until they have access to our lumberjack servers.
 # All non-ec2 clouds will use default syslog-ng configuration
 #
-if "#{node[:rightscale][:servers][:lumberjack][:hostname]}" != "" && node[:rs_utils][:enable_remote_logging] == true
 
-  log "Configure remote syslog logging"
+log 'Setup RightScale remote syslog logging.'
 
-  # == Make sure syslog-ng is installed.
-  #
-  package "syslog-ng"
+if !node.has_key? :rightscale and (node.rightscale.servers.lumberjack.hostname.nil? or node.rightscale.servers.lumberjack.hostname.empty?)
+  log 'Not attached to RightScale, skipping logging setup.'
+  return
+end
 
-  service "syslog-ng" do
-    supports :start => true, :stop => true, :restart => true
-    action [ :enable ]
-  end
+package "syslog-ng"
 
-  # == Create a new /dev/null for syslog-ng to use
-  #
-  execute "ensure_dev_null" do
-    creates "/dev/null.syslog-ng"
-    command "mknod /dev/null.syslog-ng c 1 3"
-  end
+service "syslog-ng" do
+  supports :start => true, :stop => true, :restart => true
+  action [ :enable ]
+end
 
-  # == Configure syslog
-  #
-  template "/etc/syslog-ng/syslog-ng.conf" do
-    source "syslog.erb"
-    variables ({
-      :apache_log_dir => (node[:platform] == "centos") ? "httpd" : "apache2"
-    })
-    notifies :restart, resources(:service => "syslog-ng")
-  end
+# == Create a new /dev/null for syslog-ng to use
+execute "ensure_dev_null" do
+  creates "/dev/null.syslog-ng"
+  command "mknod /dev/null.syslog-ng c 1 3"
+end
 
-  # == Ensure everything in /var/log is owned by root, not syslog.
-  #
-  Dir.glob("/var/log/*").each do |f|
-    if ::File.directory?(f)
-      
-      directory f do 
-        owner "root" 
-        notifies :restart, resources(:service => "syslog-ng")
-      end
-      
-    else
-      
-      file f do 
-        owner "root" 
-        notifies :restart, resources(:service => "syslog-ng")
-      end
-    
+# == Configure syslog
+template "/etc/syslog-ng/syslog-ng.conf" do
+  source "syslog.erb"
+  variables ({
+    :apache_log_dir => (node[:platform] == "centos") ? "httpd" : "apache2"
+  })
+  notifies :restart, resources(:service => "syslog-ng")
+end
+
+# == Ensure everything in /var/log is owned by root, not syslog.
+Dir.glob("/var/log/*").each do |f|
+  if ::File.directory?(f)
+    directory f do 
+      owner "root" 
+      notifies :restart, resources(:service => "syslog-ng")
+    end
+  else
+    file f do 
+      owner "root" 
+      notifies :restart, resources(:service => "syslog-ng")
     end
   end
-
-  # == Set up log file rotation
-  #
-  remote_file "/etc/logrotate.conf" do
-    source "logrotate.conf"
-  end
-  
-  remote_file node[:rs_utils][:logrotate_config] do
-    source "logrotate.d.syslog"
-  end
-  
-  # == Fix /var/log/boot.log issue
-  #
-  file "/var/log/boot.log" 
-
-  # == Tag required to activate logging
-  #
-  right_link_tag "rs_logging:state=active"
-  log "Setting logging active tag"
-
 end
+
+# == Set up log file rotation
+remote_file "/etc/logrotate.conf" do
+  source "logrotate.conf"
+end
+  
+remote_file node[:rs_utils][:logrotate_config] do
+  source "logrotate.d.syslog"
+end
+  
+# == Fix /var/log/boot.log issue
+file "/var/log/boot.log" 
+
+# == Tag required to activate logging
+right_link_tag "rs_logging:state=active"
+log "Setting logging active tag"
