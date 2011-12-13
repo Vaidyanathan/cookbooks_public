a3b97096f27ed04e4a7f909e5bc4a0f205ffbd37
Chris Fordham <chris@xhost.com.au>
2011-12-14 09:52:21 +1100

    Add choices for rs_utils/timezone.

--- cookbooks/rs_utils/metadata.rb
+++ cookbooks/rs_utils/metadata.rb
@@ -13,14 +13,15 @@ recipe "rs_utils::setup_ssh", "Installs private ssh key."
 recipe "rs_utils::setup_hostname", "Set system hostname."
 recipe "rs_utils::setup_timezone", "Sets system timezone."
 recipe "rs_utils::setup_server_tags", "Sets machine tags common to all RightScale managed servers."
-recipe "rs_utils::install_tools", "Install RightScale instance tools"
-recipe "rs_utils::install_mysql_collectd_plugin", "Install mysql collectd plugin"
+recipe "rs_utils::install_tools", "Install RightScale instance tools."
+recipe "rs_utils::install_mysql_collectd_plugin", "Install mysql collectd plugin."
 recipe "rs_utils::install_file_stats_collectd_plugin", "Install file-stats.rb collectd plugin.  This is used for mysql binary backup alerting."
 
 attribute "rs_utils/timezone",
   :display_name => "Timezone",
   :description => "Sets the system time to the timezone of the specified input, which must be a valid zoneinfo/tz database entry.  If the input is 'unset' the timezone will use the 'localtime' that's defined in your RightScale account under Settings -> User -> Preferences tab.  You can find a list of valid examples from the timezone pulldown bar in the Preferences tab. The server will not be updated for daylight savings time.  Ex: US/Pacific, US/Eastern",
   :required => "optional",
+  :choices => [ "US/Pacific", "US/Eastern", "Asia/Tokyo", "Australia/Sydney" ],
   :recipes => [ "rs_utils::setup_timezone" ]
   
 attribute "rs_utils/process_list",
