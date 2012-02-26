466b9000f7a3ad94c27fac2038831a0e5e098e86
Chris Fordham <chris@xhost.com.au>
2012-02-27 09:46:37 +1100

    Test right_rackspace from rubygems.org instead of local install.

--- cookbooks/rs_utils/recipes/install_tools.rb
+++ cookbooks/rs_utils/recipes/install_tools.rb
@@ -40,38 +40,14 @@ end
 
 RS_SANDBOX_GEM_BINARY="/opt/rightscale/sandbox/bin/gem"
 
-#
 # right_rackspace
-#
 gem_package "right_rackspace" do
-  source "/var/rightscale/cache/rubygems/right_rackspace-0.0.0.20111110.gem"
-  version "0.0.0.2"
   gem_binary "#{RS_SANDBOX_GEM_BINARY}"
-  action :nothing
 end
 
-remote_file "/var/rightscale/cache/rubygems/right_rackspace-0.0.0.20111110.gem" do
-  source "https://github.com/rightscale/cookbooks_public/blob/bb0d95a42f2d897768344b2430fe872bd6158a81/cookbooks/rs_utils/files/default/right_rackspace-0.0.0.20111110.gem"
-  action :nothing
-  mode "07775"
-  notifies :install, resources(:gem_package => "right_rackspace"), :delayed
-end
-
-http_request "HEAD https://github.com/rightscale/cookbooks_public/blob/bb0d95a42f2d897768344b2430fe872bd6158a81/cookbooks/rs_utils/files/default/right_rackspace-0.0.0.20111110.gem" do
-  message ""
-  url "https://github.com/rightscale/cookbooks_public/blob/bb0d95a42f2d897768344b2430fe872bd6158a81/cookbooks/rs_utils/files/default/right_rackspace-0.0.0.20111110.gem"
-  if File.exists?("/var/rightscale/cache/rubygems/right_rackspace-0.0.0.20111110.gem")
-    headers "If-Modified-Since" => File.mtime("/var/rightscale/cache/rubygems/right_rackspace-0.0.0.20111110.gem").httpdate
-  end
-  notifies :create, resources(:remote_file => "/var/rightscale/cache/rubygems/right_rackspace-0.0.0.20111110.gem"), :immediately
-end
-
-#
-# rightscale_tools_public
-#
+# rightscale_tools_public (not yet in rubygems.org repos)
 gem_package "rightscale_tools_public" do
   source "/var/rightscale/cache/rubygems/rightscale_tools_public-1.0.26.gem"
-  version "1.0.26"
   gem_binary "#{RS_SANDBOX_GEM_BINARY}"
   action :nothing
 end
