fb791f0c875e86b9e5e32e08ed53c81a04cb9062
Chris Fordham <chris@xhost.com.au>
2012-03-22 10:17:39 +1100

    Add comment header, remove markers, improve syntax.

--- cookbooks/sys_dns/recipes/do_set_private.rb
+++ cookbooks/sys_dns/recipes/do_set_private.rb
@@ -1,17 +1,12 @@
-#
 # Cookbook Name:: sys_dns
-#
+# Recipe:: do_set_private
+
 # Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-rs_utils_marker :begin
-
 sys_dns "default" do
-  id node[:sys_dns][:id]
-  address node[:cloud][:private_ips][0]
-
+  id node['sys_dns']['id']
+  address node['cloud']['private_ips'][0]
   action :set_private
 end
-
-rs_utils_marker :end
