cc4994b19207a871d9e39f9e3be063f523faf815
Chris Fordham <chris@xhost.com.au>
2012-03-23 16:30:30 +1100

    Add support for icanhazip.com when no cloud attribute is available.

--- cookbooks/sys_dns/recipes/do_set_public.rb
+++ cookbooks/sys_dns/recipes/do_set_public.rb
@@ -5,8 +5,15 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+if ! node.has_key?('cloud')
+  public_ip=`curl -s http://icanhazip.com/`.strip
+  log "Detected public IP: #{public_ip}"
+else
+  public_ip = node['cloud']['public_ips'][0]
+end
+
 sys_dns "default" do
   id node['sys_dns']['id']
-  address node['cloud']['public_ips'][0]
+  address public_ip
   action :set_public
 end
