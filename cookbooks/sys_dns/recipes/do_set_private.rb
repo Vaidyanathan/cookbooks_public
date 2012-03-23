c0225ead9c26eea21453a57e3e04c01f3e177327
Chris Fordham <chris@xhost.com.au>
2012-03-23 17:08:13 +1100

    Add include default recipe.

--- cookbooks/sys_dns/recipes/do_set_private.rb
+++ cookbooks/sys_dns/recipes/do_set_private.rb
@@ -5,6 +5,8 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+include_recipe "sys_dns"
+
 require 'socket'
 
 def local_ip
--- cookbooks/sys_dns/recipes/do_set_public.rb
+++ cookbooks/sys_dns/recipes/do_set_public.rb
@@ -5,6 +5,8 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+include_recipe "sys_dns"
+
 if ! node.has_key?('cloud')
   public_ip=`curl -s http://icanhazip.com/`.strip
   log "Detected public IP: #{public_ip}"
