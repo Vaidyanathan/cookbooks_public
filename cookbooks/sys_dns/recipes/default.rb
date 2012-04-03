104bd1910a318f3c1f2c3fe330056ab9c73198ae
Chris Fordham <chris@xhost.com.au>
2012-04-03 17:27:00 +1000

    Add in basic mac_os_x support.

--- cookbooks/sys_dns/recipes/default.rb
+++ cookbooks/sys_dns/recipes/default.rb
@@ -5,10 +5,12 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
+unless platform?('mac_os_x')
 package value_for_platform(
     [ "ubuntu", "debian" ] => { "default" => "libdigest-sha1-perl" },
     [ "centos", "redhat", "suse" ] => { "default" => "perl-Digest-SHA1" },
-    [ "archlinux" ] => { "default" => "perl-digest-sha1" }
+    [ "archlinux" ] => { "default" => "perl-digest-sha1" },
+    [ "darwin" ] => { "default" => "perl-digest-sha1" }
 )
 
 package value_for_platform(
@@ -16,10 +18,14 @@ package value_for_platform(
     [ "centos", "redhat", "suse" ] => { "default" => "perl-Digest-HMAC" },
     [ "archlinux" ] => { "default" => "perl-digest-hmac" }
 )
+  root_group = 'root'
+else
+  root_group = 'wheel'
+end
 
 directory "/opt/rightscale/dns" do
   owner "root"
-  group "root"
+  group root_group
   mode "0755"
   recursive true
 end
@@ -27,7 +33,7 @@ end
 cookbook_file "/opt/rightscale/dns/dnscurl.pl" do
   source "dnscurl.pl"
   owner "root"
-  group "root"
+  group root_group
   mode "0755"
   backup false
 end
