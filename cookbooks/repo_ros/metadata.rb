fc2cbc9aec051ec4e6797c2ddf9c22871161e6ed
Chris Fordham <chris@xhost.com.au>
2012-05-07 15:11:31 +1000

    Add in repo* cookbooks from RightScale cookbooks_public upstream master branch.

--- cookbooks/repo_ros/metadata.rb
+++ cookbooks/repo_ros/metadata.rb
@@ -1,13 +1,10 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          "Copyright RightScale, Inc. All rights reserved."
-description      "Manages the Git fast version control system"
+description      "Manages the ROS repo code download"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-depends "git"
 depends "repo"
 
-
-
-recipe  "repo_git::default", "Default pattern of loading packages"
+recipe  "repo_ros::default", "Default pattern of loading packages and resources provided"
