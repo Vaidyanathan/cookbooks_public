8e040c1a2c2959493106ae1a55b82b0bb3fd9578
Chris Fordham <chris@xhost.com.au>
2012-05-31 14:25:12 +1000

    Add temporary skip of setup_mail for archlinux.

--- cookbooks/rs_utils/recipes/setup_mail.rb
+++ cookbooks/rs_utils/recipes/setup_mail.rb
@@ -22,6 +22,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+( log "setup_mail not yet supported on archlinux" and return) if platform?('archlinux')
+
 package "postfix" do
   not_if { platform?('mac_os_x') }
 end
